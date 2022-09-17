1e95e3b9da474cd9f281d3ae667f72bee4a02b0c
Fernando Salas <luisfernandosalasg@gmail.com>
2022-09-17 17:25:31 -0500

    Add Boost

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Showcase from "./components/Showcase";
 import Advanced from "./components/Advanced";
+import Boost from "./components/Boost";
 
 function App() {
 	return (
 		<div>
 			<Showcase />
 			<Advanced />
+			<Boost />
 		</div>
 	);
 }
