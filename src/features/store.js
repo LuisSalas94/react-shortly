ac53298669152cf9b6e9ab0f1d8f0ff21e78909e
Fernando Salas <luisfernandosalasg@gmail.com>
2022-09-18 12:12:08 -0500

    Add Redux Persist

--- src/features/store.js
+++ src/features/store.js
@@ -1,10 +1,33 @@
 import { configureStore } from "@reduxjs/toolkit";
 import linkReducer from "../features/links/linkSlice";
+import {
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const persistConfig = {
+	key: "persist-link-app",
+	version: 1,
+	storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, linkReducer);
 const store = configureStore({
 	reducer: {
-		links: linkReducer,
+		links: persistedReducer,
 	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 });
 
 export default store;
--- src/index.js
+++ src/index.js
@@ -7,13 +7,19 @@ import { Provider } from "react-redux";
 //Toastify
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+//Redux Persist
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<ToastContainer />
 		<Provider store={store}>
-			<App />
+			<PersistGate loading={null} persistor={persistor}>
+				<App />
+			</PersistGate>
 		</Provider>
 	</React.StrictMode>
 );
